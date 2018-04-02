7a858a506b2be382ad3a1165bd74d6a6e06c33a6
Palmer Harrell <palmerharrell@me.com>
2018-04-02 09:44:08 -0500

    Renaming CSS files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import Router from "./components/Router";
 import "./css/normalize.css";
-import "./css/mobile.css";
+import "./css/style.css";
 import "./css/desktop.css";
 
 render(<Router />, document.querySelector("#main"));

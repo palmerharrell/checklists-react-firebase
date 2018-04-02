fb0fb38317e81ecf8cbbf8e2e735daab52ce2d65
Palmer Harrell <palmerharrell@me.com>
2018-04-02 09:38:38 -0500

    Splitting CSS into separate mobile and desktop files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import Router from "./components/Router";
 import "./css/normalize.css";
-import "./css/style.css";
+import "./css/mobile.css";
+import "./css/desktop.css";
 
 render(<Router />, document.querySelector("#main"));
